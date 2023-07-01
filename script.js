41a642985dca10c1981deeabc8ae6d976ba7fe8d
mydudethecoder <136914236+mydudethecoder@users.noreply.github.com>
2023-07-01 17:11:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const cat1 = document.querySelector("#cat1")
+const cat2 = document.querySelector("#cat2")
+const cat3 = document.querySelector("#cat3")
+const cat4 = document.querySelector("#cat4")
+const cat5 = document.querySelector("#cat5")
+function funFact1() {
+    alert("Cats are one of, if not the most, popular pet in the world.")
+}
+function funFact2() {
+    alert("There are over 500 million domestic cats in the world.")
+}
+function funFact3() {
+    alert("Cats and humans have been associated for nearly 10000 years.")
+}
+function funfact4() {
+    alert("Cats conserve energy by sleeping for an average of 13 to 14 hours a day.")
+}
+function funFact5() {
+    alert("Cats have flexible bodies and teeth adapted for hunting small animals such as mice and rats.")
+}
+cat1.onclick = funFact1
+cat2.onclick = funFact2
+cat3.onclick = funFact3
+cat4.onclick = funfact4
+cat5.onclick = funFact5
